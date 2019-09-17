c97d9264e17a4d106b7ea37fba7af9b5efd02981
Daniel <danni539953@gmail.com>
2019-09-17 15:39:37 +0000

    Final Version

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,98 +1,98 @@
 position_x = 1
 position_y = 3
 north = True
-south = False
 east = False
+south = False
 west = False
 running = True
 
 def notValid():
     print("Not a valid direction!")
 
-def direction(direction, position_x, position_y, north, south, east, west, running):
+def direction(direction, position_x, position_y, north, east, south, west, running):
     direction = direction.lower()
     if direction == "n":
         if north == True:
             position_y -= 1
-            north, south, east, west, running = options(position_x, position_y, north, south, east, west, running)
+            north, east, south, west, running = options(position_x, position_y, north, east, south, west, running)
         else:
             notValid()
     if direction == "s":
         if south == True:
             position_y += 1
-            north, south, east, west, running = options(position_x, position_y, north, south, east, west, running)
+            north, east, south, west, running = options(position_x, position_y, north, east, south, west, running)
         else:
             notValid()
     if direction == "e":
         if east == True:
             position_x += 1
-            north, south, east, west, running = options(position_x, position_y, north, south, east, west, running)
+            north, east, south, west, running = options(position_x, position_y, north, east, south, west, running)
         else:
             notValid()
     if direction == "w":
         if west == True:
             position_x -= 1
-            north, south, east, west, running = options(position_x, position_y, north, south, east, west, running)
+            north, east, south, west, running = options(position_x, position_y, north, east, south, west, running)
         else:
             notValid()
-    return position_x, position_y, north, south, east, west, running
+    return position_x, position_y, north, east, south, west, running
 
-def options(position_x, position_y, north, south, east, west, running):
+def options(position_x, position_y, north, east, south, west, running):
     if position_x == 1 and position_y == 3:
+        print("You can travel: (N)orth.")
         north = True
-        south = False
         east = False
+        south = False
         west = False
     elif position_x == 1 and position_y == 2:
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
         north = True
-        south = True
         east = True
+        south = True
         west = False
     elif position_x == 1 and position_y == 1:
+        print("You can travel: (E)ast or (S)outh.")
         north = False
-        south = True
         east = True
+        south = True
         west = False
     elif position_x == 2 and position_y == 3:
+        print("You can travel: (N)orth.")
         north = True
-        south = False
         east = False
+        south = False
         west = False
     elif position_x == 2 and position_y == 2:
+        print("You can travel: (S)outh or (W)est.")
         north = False
-        south = True
         east = False
+        south = True
         west = True
     elif position_x == 2 and position_y == 1:
+        print("You can travel: (E)ast or (W)est.")
         north = False
-        south = False
         east = True
+        south = False
         west = True
     elif position_x == 3 and position_y == 3:
-        print("WINNER")
+        print("Victory!")
         running = False
         return north, south, east, west, running
     elif position_x == 3 and position_y == 2:
+        print("You can travel: (N)orth or (S)outh.")
         north = True
-        south = True
         east = False
+        south = True
         west = False
     elif position_x == 3 and position_y == 1:
+        print("You can travel: (S)outh or (W)est.")
         north = False
-        south = True
         east = False
+        south = True
         west = True
-    return north, south, east, west, running
-        
+    return north, east, south, west, running
+
+print("You can travel: (N)orth.")
 while running:
-        print("You can travel:", end=" ")
-        if north == True:
-            print("(N)orth", end=" ")
-        if south == True:
-            print("(S)outh", end=" ")
-        if east == True:
-            print("(E)ast", end=" ")
-        if west == True:
-            print("(W)est", end=" ")
         directionInput = input("Direction: ")
-        position_x, position_y, north, south, east, west, running = direction(directionInput, position_x, position_y, north, south, east, west, running)
+        position_x, position_y, north, east, south, west, running = direction(directionInput, position_x, position_y, north, east, south, west, running)
